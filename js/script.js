4403176b71b88fdfb6d78e8fb2f4ad020cba8a5a
fedeloxe <loxeee@gmail.com>
2023-01-09 16:40:41 +0100

    base done

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const{
+    createApp
+}= Vue
+
+createApp({
+    data() { 
+        return{
+            emailList: [],
+            email:'',
+        }
+    },
+    mounted: function() {
+
+        for(let i = 0; i < 10; i++) {
+            axios
+            .get ('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((result)=> {
+                this.email = result.data.response;
+                this.emailList.push(this.email);
+            });
+        }
+
+    },
+}).mount('#app')
